1b6d467e08801d90447172ae8ae700c58c58b89e
Shu1-net <shuichi.ama@gmail.com>
2020-09-19 09:23:21 +0900

    Initial Commit

---
+++ TaxApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxApp
+//
+//  Created by 天野修一 on 2020/09/19.
+//  Copyright © 2020 shuichiama.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
